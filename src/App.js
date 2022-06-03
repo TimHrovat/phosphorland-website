d80256c6036688d7d3560916dcdb708ae719d6f5
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-03 23:14:52 +0100

    added pages & navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import ErrorPage from "./pages/ErrorPage";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Traps from "./pages/Traps";
+import "./styles.css";
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Smart Traps</Link>
-        <Link to="/home">Home</Link>
-        <Link to="/traps">Traps</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Log in</Link>
+        <div className="logo">
+          <Link to="/">Smart Traps</Link>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/home">Home</Link>{" "}
+          </li>
+          <li>
+            <Link to="/traps">Traps</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+        </ul>
+        <div className="burger">
+          <div class="line1"></div>
+          <div class="line1"></div>
+          <div class="line1"></div>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import titleImage from "../assets/home-page-images/homepage.png";
 
 function Home() {
-  return <div>home</div>;
+  return (
+    <>
+      <img src={titleImage} alt="farm landscape" className="title-image"></img>
+    </>
+  );
 }
 
 export default Home;
