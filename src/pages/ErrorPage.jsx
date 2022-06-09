2a77241960bdc95a55e95313d012dae45b3c6704
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-09 16:00:49 +0100

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  // eslint-disable-next-line
+  const [t, i18n] = useTranslation("common");
+
+  return (
+    <>
+      <div className="title-slide">
+        <div className="title">
+          <h1 className="home-heading">{t("error.title")}</h1>
+          <Link to="/home">
+            <button className="contact-us-button">{t("error.button")}</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
