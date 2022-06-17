b1d68b7c11979567af9eec0aae6aaf1e588bd74d
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-17 16:58:00 +0100

    added calendar

--- src/components/dashboard-content/DashboardContent.jsx
+++ src/components/dashboard-content/DashboardContent.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./dashboard-content.css";
+import "react-calendar/dist/Calendar.css";
+import Calendar from "react-calendar";
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import firebase from "firebase/compat/app";
 
 export default function DashboardContent() {
+  const [date, setDate] = useState(new Date());
+  const [displayDate, setDisplayDate] = useState(true);
+  const [allImages, setImages] = useState([]);
+
+  function loadImages() {
+    if (!displayDate) return;
+
+    setImages([]);
+
+    const path =
+      "images/" + firebase.auth().currentUser.uid + "/" + date.toDateString();
+
+    let storageRef = ref(getStorage(), path);
+
+    listAll(storageRef)
+      .then(function (res) {
+        res.items.forEach((imageRef) => {
+          getDownloadURL(imageRef).then((url) => {
+            setImages((allImages) => [...allImages, url]);
+          });
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <>
-      <main></main>
+      <main>
+        <section
+          style={displayDate ? { display: "block" } : { display: "none" }}
+        >
+          <h1>Select the date to see the images:</h1>
+          <div className="calendar-container">
+            <Calendar onChange={setDate} value={date} />
+          </div>
+          <div>Selected date: {date.toDateString()}</div>
+          <button
+            onClick={() => {
+              setDisplayDate(false);
+              loadImages();
+            }}
+          >
+            change
+          </button>
+        </section>
+        <section
+          style={!displayDate ? { display: "block" } : { display: "none" }}
+        >
+          {allImages.map((image) => {
+            return (
+              <div key={image} className="image">
+                <img src={image} alt="" style={{ width: 300, height: 300 }} />
+              </div>
+            );
+          })}
+          <button
+            onClick={() => {
+              setDisplayDate(true);
+            }}
+          >
+            change
+          </button>
+        </section>
+      </main>
     </>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 });
 
 export const auth = app.auth();
