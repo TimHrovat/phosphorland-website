82473a50c3038f22347ae99426fa8af394b9e5f9
Tim Hrovat <tim.hrovat16@gmail.com>
2022-06-22 20:39:19 +0100

    .env check

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,19 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+if (
+  !(
+    process.env.REACT_APP_FIREBASE_API_KEY &&
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN &&
+    process.env.REACT_APP_FIREBASE_PROJECT_ID &&
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET &&
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID &&
+    process.env.REACT_APP_FIREBASE_APP_ID &&
+    process.env.REACT_APP_FIREBASE_DATABASE_URL
+  )
+)
+  alert("please configure your .env file correctly");
+
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
